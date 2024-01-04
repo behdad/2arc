fb41a4cab3352772f62e7029400634ca49d3ff2d
Behdad Esfahbod <behdad@behdad.org>
2024-01-04 06:50:59 -0700

    Minor

--- 2arcs.py
+++ 2arcs.py
@@ -34,12 +34,9 @@ def lines_2arc_connection(l0, l1):
 
     # Find the centers of the arcs
     rotate90 = complex(0, 1)
-    c0 = lines_intersection(
-        l0[1], d0 * rotate90, arcs_meeting_point, connection * rotate90
-    )
-    c1 = lines_intersection(
-        l1[0], d1 * rotate90, arcs_meeting_point, connection * rotate90
-    )
+    connection_normal = connection * rotate90
+    c0 = lines_intersection(l0[1], d0 * rotate90, arcs_meeting_point, connection_normal)
+    c1 = lines_intersection(l1[0], d1 * rotate90, arcs_meeting_point, connection_normal)
     if c0 is None or c1 is None:
         # Single-arc solution
         c0 = c1 = l0[1] + d0r
